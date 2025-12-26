a04bf95b3c49bcb6b183db08b903d572fcb34d1d
Trent1337 <jonas.trentmann@hotmail.com>
2025-12-26 21:14:15 +0100

    Update vrfrag_teams.py

--- vrfrag_teams.py
+++ vrfrag_teams.py
@@ -185,7 +185,7 @@ def generate_fair_teams(player_names, player_stats_df, map_name=None, max_iterat
     best_fairness = float('inf')
     iterations_used = 0
 
-    TOP_K = 25          # wie viele gute Kandidaten wir sammeln
+    TOP_K = 10          # wie viele gute Kandidaten wir sammeln
     EPS = 0.015          # wie nah an best_fairness noch akzeptiert (1.5% vom Gesamtscore)
 
     
